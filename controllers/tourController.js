5ce80ca30f6f8da13c8303adff7e4cddabcedf5c
DmitryKn <4dmitry.kn@gmail.com>
2020-06-04 13:47:13 -0400

    mongo db connection

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,64 +1,95 @@
-const fs = require('fs');
-const toursData = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/tours.json`)
-);
+const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: toursData.length,
-        data: {
-            tours: toursData,
-        },
-    });
-};
+exports.getAllTours = async (req, res) => {
+    try {
+        const allTours = await Tour.find();
 
-exports.getTour = (req, res) => {
-    const id = req.params.id * 1;
-    const tour = toursData.find((el) => el.id === id);
-    //if (id > toursData.length) {
-    if (!tour) {
-        return res.status(404).json({
+        res.status(200).json({
+            status: 'success',
+            result: allTours.length,
+            data: {
+                tours: allTours,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
             status: 'fail',
-            message: 'Invalid ID',
+            message: err,
         });
     }
-
-    res.status(200).json({
-        status: 'success',
-        results: toursData.length,
-        data: {
-            tours: tour,
-        },
-    });
 };
 
-exports.createTour = (req, res) => {
-    const newId = toursData[toursData.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
+exports.getTour = async (req, res) => {
+    try {
+        const tour = await Tour.findById(req.params.id);
 
-    toursData.push(newTour);
-
-    fs.writeFile('../dev-data/tours', JSON.stringify(toursData), (err) => {
         res.status(201).json({
             status: 'success',
             data: {
-                tours: newTour,
+                tour: tour,
             },
         });
-    });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err,
+        });
+    }
 };
 
-exports.updateTour = (req, res) => {
-    res.status(200).json({
-        data: {
-            tour: '<Updated tour here>',
-        },
-    });
+exports.createTour = async (req, res) => {
+    try {
+        const newTour = await Tour.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err,
+        });
+    }
 };
 
-exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        data: null,
-    });
+exports.updateTour = async (req, res) => {
+    try {
+        const updatedTour = await Tour.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour: updatedTour,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+};
+
+exports.deleteTour = async (req, res) => {
+    try {
+        await Tour.findByIdAndDelete(req.params.id);
+
+        res.status(204).json({
+            data: null,
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
 };
--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-app.listen(8000, () => {
+dotenv.config({ path: './config.env' });
+
+//DB connection
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('DB connections successful !'));
+
+//  Server starts
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('Server runs port 8000...');
 });
