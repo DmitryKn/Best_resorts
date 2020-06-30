bb96dd4616dfae87c7511ad0dfd42045786634dc
DmitryKn <4dmitry.kn@gmail.com>
2020-06-29 21:46:12 -0400

    db error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,60 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path} : ${err.value}.`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsErrorDB = (err) => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
+    const message = `Duplicate field value: ${value}.  Please use another value!`;
+    return new AppError(message, 400);
+}
+
+const handleValidationError = (err) => {
+    const errors = Object.values(err.errors).map(el => el.message)
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+}
+const sendErrorDevelopment = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
+    });
+};
+const sendErrorProduction = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        console.error('ERROR', err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!',
+        });
+    }
+};
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDevelopment(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        let error = { ...err };
+
+        // get wrong id error
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        //duplicate db fields error
+        if(error.code === 11000) error = handleDuplicateFieldsErrorDB(error);
+        //mongoose validation errors(wrong updates)
+        if(error.name ==='ValidationError') error = handleValidationError(error)
+
+        sendErrorProduction(error, res);
+    }
 };
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = async (req, res, next) => {
     req.query.limit = '5';
@@ -31,8 +32,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id);
 
     if (!tour) {
-        next(new AppError('No tour found with that id'));
+        return next(new AppError('No tour found with that ID', 404));
     }
+
     res.status(201).json({
         status: 'success',
         data: {
@@ -57,6 +59,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
         new: true,
         runValidators: true,
     });
+
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404));
+    }
+
     res.status(200).json({
         status: 'success',
         data: {
@@ -66,7 +73,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-    await Tour.findByIdAndDelete(req.params.id);
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404));
+    }
 
     res.status(204).json({
         data: null,
